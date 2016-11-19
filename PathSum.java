1c1a40540560b9717e456767a4a6955a2569825d
Jeffery Liu <jeffery300@gmail.com>
2016-11-19 15:56:12 -0500

    Added variable to PathSum

--- PathSum.java
+++ PathSum.java
@@ -2,6 +2,7 @@
     public class PathSum {
         public int hasPath(int target, TreeNode tree){
             // replace with working code
+        	int jeffery = 0
         	if(tree ==  null)
         		return 0;
         	if((tree.left == null && tree.right == null) && tree.info == target)
