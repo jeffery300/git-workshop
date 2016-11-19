1b2d1ea01e5efdf3c0be7cebc1b0013d349cc09e
Jeffery Liu <jeffery300@gmail.com>
2016-11-19 15:49:19 -0500

    new file

---
+++ PathSum.java
@@ -0,0 +1,13 @@
+
+    public class PathSum {
+        public int hasPath(int target, TreeNode tree){
+            // replace with working code
+        	if(tree ==  null)
+        		return 0;
+        	if((tree.left == null && tree.right == null) && tree.info == target)
+        		return 1;
+        	return hasPath(target-tree.info,tree.left) + hasPath(target-tree.info,tree.right);
+        }
+    }
+    
+    
